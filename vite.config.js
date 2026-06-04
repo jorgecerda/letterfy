e0c476be0755990d039d830255b277c3fa107525
jorgecerda <jorgecerda@github.com>
2026-06-04 01:33:18 -0600

    Enable HTTPS SSL configuration and bind host to 0.0.0.0 for secure mobile testing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    basicSsl()
   ],
   server: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 5174,
     strictPort: false,
     proxy: {
